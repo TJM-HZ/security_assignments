c8c09aaf2598cabb73ac53b5c0ddd4be3d3cab52
TJM-HZ <90186564+TJM-HZ@users.noreply.github.com>
2022-06-20 10:17:38 +0200

    Added auth middleware to posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('auth', ['except' => ['index','show']]);
+    }
+
+
     /**
      * Display a listing of the resource.
      *
