747024b5f58ff68b8206281d608641fbe7a82841
TJM-HZ <90186564+TJM-HZ@users.noreply.github.com>
2022-07-05 23:30:07 +0200

    Link to article added to password reset

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,7 +5,7 @@
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
-
+        <b class="underline text-blue-500"><a href="/password-managers">Please consider using a password manager for easier and more secure registration and login</a></b>
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
@@ -38,6 +38,18 @@
                                     name="password_confirmation" required />
             </div>
 
+                <div class="mt-4">
+                    Password Requirements:
+                    <ul class="list-disc text-red-600">
+                        <li id="minChar">At least 16 characters</li>
+                        <li id="lowercase">At least 1 <i>lowercase letter</i></li>
+                        <li id="uppercase">At least 1 <i>UPPERCASE LETTER</i></li>
+                        <li id="number">At least 1 <i>number (0 - 9)</i></li>
+                        <li id="symbol">At least 1 <i>symbol (e.g., ! @ # $ % ^ & * ; : )</i></li>
+                        <li id="match">Passwords must match</li>
+                    </ul>
+                </div>
+
             <div class="flex items-center justify-end mt-4">
                 <x-button>
                     {{ __('Reset Password') }}
@@ -46,3 +58,61 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+<script>
+    var password = document.getElementById("password");
+    var password_c = document.getElementById("password_confirmation");
+
+    var li_minChar = document.getElementById("minChar");
+    var li_lowercase = document.getElementById("lowercase");
+    var li_uppercase = document.getElementById("uppercase");
+    var li_number = document.getElementById("number");
+    var li_symbol = document.getElementById("symbol");
+    var li_match = document.getElementById("match");
+
+    var clr_ok = "text-green-600";
+    var clr_error = "text-red-600";
+
+    document.addEventListener('keyup', function () {
+        // Check password length
+        if (password.value.length >= 16) {
+            li_minChar.className = clr_ok;
+        } else {
+            li_minChar.className = "text-inherit";
+        }
+
+        // Check if password contains a lowercase character
+        if (/[a-z]/.test(password.value)) {
+            li_lowercase.className = clr_ok;
+        } else {
+            li_lowercase.className = "text-inherit";
+        }
+
+        // Check if password contains an UPPERCASE character
+        if (/[A-Z]/.test(password.value)) {
+            li_uppercase.className = clr_ok;
+        } else {
+            li_uppercase.className = "text-inherit";
+        }
+
+        // Check if password contains a number
+        if (/\d/.test(password.value)) {
+            li_number.className = clr_ok;
+        } else {
+            li_number.className = "text-inherit";
+        }
+
+        // Check if password contains a special character
+        if (/[^a-zA-Z0-9]/.test(password.value)) {
+            li_symbol.className = clr_ok;
+        } else {
+            li_symbol.className = "text-inherit";
+        }
+
+        // Check if passwords match
+        if (password.value === password_c.value && password.value.length > 0) {
+            li_match.className = clr_ok;
+        } else {
+            li_match.className = "text-inherit";
+        }
+    });
+</script>
